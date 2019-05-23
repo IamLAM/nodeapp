d1c01d93377a98045ba406e0800bea17bd28bffe
Glitch (nodesapp) <none>
2019-05-23 03:12:57 +0000

    ❣️👚 Working with passport and express session

--- server.js
+++ server.js
@@ -3,8 +3,20 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-
+const passport  = require('passport');
+const session= require('express-session');
 const app = express();
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(session.initialize());
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
