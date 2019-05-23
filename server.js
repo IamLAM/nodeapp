164a71f105b3bdc984e23d480843ad386cef523a
Glitch (nodesapp) <none>
2019-05-23 05:39:59 +0000

    🎂👟another server.js

--- server.js
+++ server.js
@@ -1,125 +1,79 @@
 'use strict';
 
-const routes = require('./routes.js');
-const auth = require('./auth.js');
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const session     = require('express-session');
-const mongodb     = require('mongodb');
-const mongo = require('mongodb').MongoClient;
-const GitHubStrategy = require('passport-github').Strategy;
-// const io = require('socket.io')(http);
-const LocalStrategy = require('passport-local');
-const passport    = require('passport');
-const bcrypt     = require('bcrypt');
+const express       = require( 'express' );
+const bodyParser    = require( 'body-parser' );
+const session       = require( 'express-session' );
+const passport      = require( 'passport' );
+const localStrategy = require( 'passport-local' );
+const mongo         = require( 'mongodb' ).MongoClient;
+const objectID      = require( 'mongodb' ).ObjectID;
+const fccTesting    = require( './freeCodeCamp/fcctesting.js' );
 
-const app = express();
+const app         = express( );
 
+fccTesting( app ); //For FCC testing purposes
+app.use( '/public', express.static( process.cwd( ) + '/public' ) );
+app.use( bodyParser.json( ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( session( {
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+} ) );
+app.use( passport.initialize( ) );
+app.use( passport.session( ) );
 
+mongo.connect( process.env.DATABASE, { useNewUrlParser: true }, ( error,db ) => {
+  if ( error ) {
+    console.log( 'Database error: ' + error );
+  } else {
+    console.log( 'Successful database connection' );
 
-
-
-
-
-
-const cors = require('cors');
-app.use(cors());
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-// set 'pug' as the 'view-engine'. 
-app.set('view engine', 'pug');
-
-mongo.connect(process.env.DATABASE, (err, db) => {
-    if(err) {
-        console.log('Database error: ' + err);
-    } else {
-      console.log('Successful database connection');
-      
-      passport.use(new GitHubStrategy({
-          clientID: process.env.GITHUB_CLIENT_ID,
-          clientSecret: process.env.GITHUB_CLIENT_SECRET,
-          callbackURL: 'https://nodesapp.glitch.me/auth/github/callback'
-        },
-        function(accessToken, refreshToken, profile, cb) {
-            console.log(profile);
-            //Database logic here with callback containing our user object
-            db.collection('socialusers').findAndModify(
-              {id: profile.id},
-              {},
-              {$setOnInsert:{
-                  id: profile.id,
-                  name: profile.displayName || 'John Doe',
-                  photo: profile.photos[0].value || '',
-                  email: profile.emails[0].value || 'No public email',
-                  created_on: new Date(),
-                  provider: profile.provider || ''
-              },$set:{
-                  last_login: new Date()
-              },$inc:{
-                  login_count: 1
-              }},
-              {upsert:true, new: true},
-              (err, doc) => {
-                  return cb(null, doc.value);
-              }
-          );
-        }
-      ));
-      
-      auth(app, db)
-      routes(app, db)
-  
-    passport.use(new GitHubStrategy({
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: 'https://nodesapp.glitch.me/auth/github/callback'
-      },
-      function(accessToken, refreshToken, profile, cb) {
-          console.log(profile);
-          //Database logic here with callback containing our user object
-          db.collection('socialusers').findAndModify(
-            {id: profile.id},
-            {},
-            {$setOnInsert:{
-                id: profile.id,
-                name: profile.displayName || 'John Doe',
-                photo: profile.photos[0].value || '',
-                email: profile.emails[0].value || 'No public email',
-                created_on: new Date(),
-                provider: profile.provider || ''
-            },$set:{
-                last_login: new Date()
-            },$inc:{
-                login_count: 1
-            }},
-            {upsert:true, new: true},
-            (err, doc) => {
-                return cb(null, doc.value);
-            }
-        );
+    passport.use( new localStrategy(
+      ( username,password,done ) => {
+        db.db( ).collection( 'users' )
+          .findOne( { username: username }, (error,user ) => {
+            console.log( 'User '+ username +' attempted to log in.' );
+            if ( error )                      return done( error );
+            if ( !user )                      return done( null,false );
+            if ( password !== user.password ) return done( null,false );
+            return done( null,user );
+          } );
       }
-    ));
-      app.route('/auth/github')
-        .get((req, res) => {
-          res.redirect(passport.authenticate('github'))
-        });
+    ) );
+    
+    passport.serializeUser( ( user,done ) => {
+      done( null, user._id );
+    } );
 
-      app.route('/auth/github/callback')
-        .get(passport.authenticate('github', { failureRedirect: '/' }),(req,res) => {
-             res.redirect('/profile');
-        });
-      
-      app.listen(process.env.PORT || 3000, () => {
-          console.log("Listening on port " + process.env.PORT);
-        });
+    passport.deserializeUser( ( id,done ) => {
+      db.db( ).collection( 'users' ).findOne(
+        { _id: new objectID( id ) },
+        ( error,doc ) => {
+            done( null, doc);
+        }
+      );
+    } );
 
-    }
-});
+    app.set( 'view engine', 'pug'         );
+    app.set( 'views'      , './views/pug' );
 
+    app.get( '/', ( req,res ) => {
+      // Changed title to "Home page" so the test passed successfully.
+      res.render( 'index', { title: 'Home page', message: 'Please login', showLogin: true } );
+    } );
+    
+    app.get( '/profile', ( req,res ) => {
+      res.render( 'profile' );
+    } );
 
+    app.post(
+      '/login',
+      passport.authenticate( 'local', { successRedirect: '/profile', failureRedirect: '/' } )
+    );
+
+    app.listen( process.env.PORT || 3000, ( ) => {
+      console.log( 'Listening on port ' + process.env.PORT );
+    } );
+  }
+} );
